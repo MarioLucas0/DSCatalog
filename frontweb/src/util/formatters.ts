dee384b4e0ed13e494cd3777a48cbdeb9380b868
Lucas <mariolucas.2000@outlook.com>
2022-07-28 16:07:26 -0300

    format Price

---
+++ frontweb/src/util/formatters.ts
@@ -0,0 +1,8 @@
+export const formatPrice = (price: number) => {
+  const params = {
+    maximumFractionDigits: 2,
+    minimumFractionDigits: 2
+  }
+
+  return new Intl.NumberFormat('pt-BR', params).format(price)
+}
