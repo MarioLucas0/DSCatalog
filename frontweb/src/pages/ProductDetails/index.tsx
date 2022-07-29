98cba51161ed70b844be6b214fec3541d65b3044
Lucas <mariolucas.2000@outlook.com>
2022-07-28 21:55:16 -0300

    Product Datails mobile

--- frontweb/src/pages/ProductDetails/index.tsx
+++ frontweb/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,43 @@
+import { ReactComponent as ArrowIcon } from 'assets/img/arrow.svg'
+import ProductPrice from 'components/ProductPrice'
+import './style.css'
+
 const ProductDetails = () => {
-  return <h1>Pagina ProductDetails</h1>
+  return (
+    <section className="product-container-details">
+      <div className="base-card product-datails-card">
+        <div className="goback-container">
+          <ArrowIcon />
+          <h2>VOLTAR</h2>
+        </div>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg"
+                alt=""
+              />
+            </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descriçao do produto</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+                nostrum cupiditate aliquid temporibus, modi quod obcaecati
+                harum, dolores velit ipsam error odio recusandae perspiciatis
+                laboriosam, delectus totam similique. Earum, dolores!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default ProductDetails
